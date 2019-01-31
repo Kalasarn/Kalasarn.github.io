b9efbd916351f0c83a0e62b08d4612a261d48418
Kalasarn <njoh@live.se>
2019-01-31 22:01:59 +0100

    Updates

---
+++ precache-manifest.5e1e241fe5007311fc3d567fdb6b7ab1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "927d4d6ce3f880826736",
+    "url": "/static/css/main.8fc9b198.chunk.css"
+  },
+  {
+    "revision": "927d4d6ce3f880826736",
+    "url": "/static/js/main.927d4d6c.chunk.js"
+  },
+  {
+    "revision": "8f31718955d03fe9cf67",
+    "url": "/static/js/1.8f317189.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ee6d1fbe3c4593e7cb86bbae6fc9348",
+    "url": "/static/media/bg.0ee6d1fb.jpeg"
+  },
+  {
+    "revision": "cfb0d35eebf90b869f408b13dd916c2a",
+    "url": "/index.html"
+  }
+];
