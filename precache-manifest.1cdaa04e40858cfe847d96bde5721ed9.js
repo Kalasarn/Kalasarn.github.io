e4d5bbb81acd63c72a124c046b25f0a14071fa33
Niklas Johansson <njoh@live.se>
2019-09-26 11:18:17 +0200

    Updates

---
+++ precache-manifest.1cdaa04e40858cfe847d96bde5721ed9.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2fa887b5eebdd8fd2ebfda2e245c0c8",
+    "url": "/Kalasarn.github.io/index.html"
+  },
+  {
+    "revision": "32f7e50579f4600904c6",
+    "url": "/Kalasarn.github.io/static/css/main.e12af9f2.chunk.css"
+  },
+  {
+    "revision": "5282929735aac76b5b72",
+    "url": "/Kalasarn.github.io/static/js/2.c137de4c.chunk.js"
+  },
+  {
+    "revision": "32f7e50579f4600904c6",
+    "url": "/Kalasarn.github.io/static/js/main.738380a8.chunk.js"
+  },
+  {
+    "revision": "3e6dbeba4df897248b92",
+    "url": "/Kalasarn.github.io/static/js/runtime-main.77c3b71d.js"
+  },
+  {
+    "revision": "8bfb3bbae458c356834e62c2838b84c4",
+    "url": "/Kalasarn.github.io/static/media/backgroundimage.8bfb3bba.jpg"
+  },
+  {
+    "revision": "0ee6d1fbe3c4593e7cb86bbae6fc9348",
+    "url": "/Kalasarn.github.io/static/media/bg.0ee6d1fb.jpeg"
+  },
+  {
+    "revision": "4fc408f583b58eb0b32866aa0c746b34",
+    "url": "/Kalasarn.github.io/static/media/drops.4fc408f5.png"
+  },
+  {
+    "revision": "91ee7d0a7f76f89c938487fc681d5257",
+    "url": "/Kalasarn.github.io/static/media/snowflake.91ee7d0a.png"
+  },
+  {
+    "revision": "bcb3306a08b8a1c94207d7b0c2113acf",
+    "url": "/Kalasarn.github.io/static/media/sunny.bcb3306a.png"
+  }
+]);
